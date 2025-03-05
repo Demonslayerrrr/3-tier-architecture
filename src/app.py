0518e97fe68852d7e02c3eb77a1240da502b2021
Demonslayerrrr <u132_ilinai_waw@technischools.com>
2025-03-05 17:58:25 +0100

    hjjh

--- src/app.py
+++ src/app.py
@@ -8,42 +8,19 @@ controller = controller()
 
 @app.get("/users")
 def get_users():
-    return jsonify(controller.get_users()), HTTPStatus.OK
-
+    return controller.get_users()
 @app.get("/users/<int:user_id>")
 def get_user(user_id):
-    try:
-        return jsonify(controller.get_user(user_id)), HTTPStatus.OK
-    except KeyError:
-        return jsonify({"message": "User not found"}), HTTPStatus.NOT_FOUND
-    
+    return controller.get_user(user_id)
+   
 @app.post("/users")
 def add_user():
-    try:
-        user_info = request.json
-        controller.add_user(user_info)
-        return jsonify({"message": "User added"}), HTTPStatus.CREATED
-    except ValueError:
-        return jsonify({"message": "Group does not exist"}), HTTPStatus.BAD_REQUEST
-    
+    return controller.add_user(request.json)
 @app.patch("/users/<int:user_id>")
 def patch_user(user_id):
-    try:
-        user_modify = request.json
-        controller.patch_user(user_id,user_modify)
-        return jsonify({"message": "User info modified"}),HTTPStatus.OK
-    except KeyError:
-        return jsonify({"message": "User not found"}), HTTPStatus.NOT_FOUND
-    except ValueError:
-        return jsonify({"message": "Group does not exist"}), HTTPStatus.BAD_REQUEST
-
+    return controller.patch_user(user_id, request.json)
 @app.delete("/users/<int:user_id>")
 def delete_user(user_id):
-    try:
-        controller.delete_user(user_id)
-        return jsonify({"message": "User deleted"}), HTTPStatus.OK
-    
-    except KeyError:
-        return jsonify({"message": "User not found"}), HTTPStatus.NOT_FOUND
+    return controller.delete_user(user_id)
 if __name__ == "__main__": 
     app.run(debug=True)
--- src/controller.py
+++ src/controller.py
@@ -1,20 +1,48 @@
 from src.repository import UserRepository
+from http import HTTPStatus
+from flask import jsonify, request
 
 class UserController:
     def __init__(self) -> None:
         self.repository = UserRepository()
     
     def add_user(self, user_info:dict) -> None:
-        self.repository.add_user(user_info)
+        try:
+            user_info = request.json
+            self.repository.add_user(user_info)
+            return jsonify({"message": "User added"}), HTTPStatus.CREATED
+        except ValueError:
+            return jsonify({"message": "Group does not exist"}), HTTPStatus.BAD_REQUEST
+    
+
     
     def get_users(self) -> dict:
-        return self.repository.get_users()
+        return jsonify(self.repository.get_users()), HTTPStatus.OK
     
     def get_user(self, user_id:int) -> dict:
-        return self.repository.get_user(user_id)
+        try:
+            return jsonify(self.repository.get_user(user_id)), HTTPStatus.OK
+
+        except KeyError:
+            return jsonify({"message": "User not found"}), HTTPStatus.NOT_FOUND
+
 
     def patch_user(self, user_id:int, user_modify:dict) -> None:
-        self.repository.patch_user(user_id,user_modify)
+        try:
+            user_modify = request.json
+            self.repository.patch_user(user_id,user_modify)
+            return jsonify({"message": "User info modified"}),HTTPStatus.OK
+        except KeyError:
+            return jsonify({"message": "User not found"}), HTTPStatus.NOT_FOUND
+        except ValueError:
+            return jsonify({"message": "Group does not exist"}), HTTPStatus.BAD_REQUEST
+
+
 
     def delete_user(self, user_id: int) -> None:
-        self.repository.delete_user(user_id)
+        try:
+            self.repository.delete_user(user_id)
+            return jsonify({"message": "User deleted"}), HTTPStatus.OK
+    
+        except KeyError:
+            return jsonify({"message": "User not found"}), HTTPStatus.NOT_FOUND
